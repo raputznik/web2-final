08a4bbc4b128b413926aa4812e6c4263b5932116
Jeremy Dahl <jeremy@palebluedot.ca>
2013-07-21 23:09:02 -0700

    Got jQuery working to show/hide sections
    Got jQuery working to show/hide sections; refactored jQuery to hide
    inactive sections on one line; refactored "display: none" in CSS and
    "section" to make changes globally, with less code; removed unused
    Google Font references.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-	//fuck you!
-	$("#button").click(function(){
-	$(".poll").toggle();
+	$("#about").click(function(){
+		$(".about").fadeIn("fast");
+		$(".contact, .poll, .results").hide();
 	});
+
+	$("#contact").click(function(){
+		$(".contact").fadeIn("fast");
+		$(".about, .poll, .results").hide();
+	});
+
+	$("#poll").click(function(){
+		$(".poll").fadeIn("fast");
+		$(".about, .contact, .results").hide();
+	});
+
+	$("#results").click(function(){
+		$(".results").fadeIn("fast");
+		$(".about, .poll, .contact").hide();
+	});
+
+
+
 });
