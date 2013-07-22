1ea86b01df3c808829c292f6095eb681eb18c08d
Jeremy Dahl <jeremy@palebluedot.ca>
2013-07-21 17:13:57 -0700

    Experimenting with php poll code.
    Trying to get $_POST to work with array.

---
+++ scripts/form-process.php
@@ -0,0 +1,13 @@
+<?php 
+
+$votes = $_POST["vote1"];
+$votes = $_POST["vote2"];
+$votes = $_POST["vote3"];
+$votes = $_POST["vote4"];
+// $email = $_POST["email"];
+
+// $total = 0;
+
+var_dump($_POST);
+
+ ?>
